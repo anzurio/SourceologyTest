4b123274006964d7bde6527a7619db1520f3249a
Mario Parra <anzurio@gmail.com>
2022-07-19 19:22:43 -0500

    Add implementation and tests

---
+++ SymmetricCharacters/SymmetricCharacterValidator.cs
@@ -0,0 +1,37 @@
+﻿namespace SourceologyTest
+{
+    public class SymmetricCharacterValidator
+    {
+        private readonly SymmetricCharacterRules _rules;
+
+        public SymmetricCharacterValidator(SymmetricCharacterRules rules)
+        {
+            _rules = rules;
+        }
+
+        public bool Validate(string input)
+        {
+            var stack = new Stack<char>(); 
+            foreach (var c in input)
+            {
+                if (_rules.Opens(c))
+                {
+                    stack.Push(c);
+                }
+                else if (_rules.Closes(c))
+                {
+                    if (stack.Any() && _rules.Correspond(stack.Peek(), c))
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
---
+++ SymmetricCharactersTests/SymmetricCharacterValidatorTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SourceologyTest.Tests
+{
+    [TestClass()]
+    public class SymmetricCharacterValidatorTests
+    {
+        [TestMethod("Sample Test Cases for Braces Only")]
+        [DataRow("{}", true)]
+        [DataRow("}{", false)]
+        [DataRow("{}}", false)]
+        [DataRow("", true)]
+        [DataRow("{abc...xyz}", true)]
+        public void ProvidedBracesTests(string input, bool expectedResult)
+        {
+            var rules = new SymmetricCharacterRules(new[] { SymmetricCharacterRule.Braces });
+            var bracesValidator = new SymmetricCharacterValidator(rules);
+            Assert.AreEqual(expectedResult, bracesValidator.Validate(input));
+        }
+
+        [TestMethod("Negative Tests")]
+        [DataRow(")(")]
+        [DataRow("}{")]
+        [DataRow("][")]
+        [DataRow(")")]
+        [DataRow("(]")]
+        [DataRow("(()}{}")]
+        [DataRow("(a(a)}aaaa{}...")]
+        public void NegativeTests(string input)
+        {
+            var rules = new SymmetricCharacterRules();
+            rules.AddRule(SymmetricCharacterRule.Braces)
+                .AddRule(SymmetricCharacterRule.Brackets)
+                .AddRule(SymmetricCharacterRule.Parenthesis);
+
+            var validator = new SymmetricCharacterValidator(rules);
+            Assert.IsFalse(validator.Validate(input));
+        }
+
+        [TestMethod("Positive Tests")]
+        [DataRow("()[]{}")]
+        [DataRow("([{}])")]
+        [DataRow("([{}])")]
+        [DataRow("([{}])(){}[()]")]
+        [DataRow("(a[b{c}d]e)f(g){h}[i()]")]
+        public void PositiveTests(string input)
+        {
+            var rules = new SymmetricCharacterRules();
+            rules.AddRule(SymmetricCharacterRule.Braces)
+                .AddRule(SymmetricCharacterRule.Brackets)
+                .AddRule(SymmetricCharacterRule.Parenthesis);
+
+            var validator = new SymmetricCharacterValidator(rules);
+            Assert.IsTrue(validator.Validate(input));
+        }
+    }
+}
