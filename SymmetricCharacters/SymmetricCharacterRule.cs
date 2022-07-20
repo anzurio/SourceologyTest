50762db71a5cb35cf5a35cb1291e2b7a716f96da
Mario Parra <anzurio@gmail.com>
2022-07-19 20:15:45 -0500

    Fix recreation of constants on access

--- SymmetricCharacters/SymmetricCharacterRule.cs
+++ SymmetricCharacters/SymmetricCharacterRule.cs
@@ -2,12 +2,17 @@
 {
     public readonly struct SymmetricCharacterRule
     {
+        private static readonly SymmetricCharacterRule braces = new SymmetricCharacterRule() { Opening = '{', Closing = '}' };
+        private static readonly SymmetricCharacterRule parenthesis = new SymmetricCharacterRule() { Opening = '(', Closing = ')' };
+        private static readonly SymmetricCharacterRule brackets = new SymmetricCharacterRule() { Opening = '[', Closing = ']' };
+        private static SymmetricCharacterRule chevrons = new SymmetricCharacterRule() { Opening = '⟨', Closing = '⟩' };
+
         public readonly char Opening { get; init; }
         public readonly char Closing { get; init; }
 
-        public static SymmetricCharacterRule Braces = new SymmetricCharacterRule() { Opening = '{', Closing = '}' };
-        public static SymmetricCharacterRule Parenthesis = new SymmetricCharacterRule() { Opening = '(', Closing = ')' };
-        public static SymmetricCharacterRule Brackets = new SymmetricCharacterRule() { Opening = '[', Closing = ']' };
-        public static SymmetricCharacterRule Chevrons = new SymmetricCharacterRule() { Opening = '⟨', Closing = '⟩' };
+        public static SymmetricCharacterRule Braces => braces;
+        public static SymmetricCharacterRule Parenthesis => parenthesis;
+        public static SymmetricCharacterRule Brackets => brackets;
+        public static SymmetricCharacterRule Chevrons => chevrons;
     }
 }
