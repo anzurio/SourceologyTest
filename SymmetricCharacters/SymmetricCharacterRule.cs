523754bfd5fe1aa47db4a26377d1853ae21e104e
Mario Parra <anzurio@gmail.com>
2022-07-19 20:16:16 -0500

    Missing `readonly` modifier in one constant

--- SymmetricCharacters/SymmetricCharacterRule.cs
+++ SymmetricCharacters/SymmetricCharacterRule.cs
@@ -5,7 +5,7 @@
         private static readonly SymmetricCharacterRule braces = new SymmetricCharacterRule() { Opening = '{', Closing = '}' };
         private static readonly SymmetricCharacterRule parenthesis = new SymmetricCharacterRule() { Opening = '(', Closing = ')' };
         private static readonly SymmetricCharacterRule brackets = new SymmetricCharacterRule() { Opening = '[', Closing = ']' };
-        private static SymmetricCharacterRule chevrons = new SymmetricCharacterRule() { Opening = '⟨', Closing = '⟩' };
+        private static readonly SymmetricCharacterRule chevrons = new SymmetricCharacterRule() { Opening = '⟨', Closing = '⟩' };
 
         public readonly char Opening { get; init; }
         public readonly char Closing { get; init; }
